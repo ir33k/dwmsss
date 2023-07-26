e91e930ed03968d2791b705cd1c604029c6cad9e
irek <irek@gabr.pl>
2023-07-26 20:51:15 +0200

    Fatality! v2
    After last refactor I noticed that most of the functions are just
    there to read single word from specific file.  So if there was single
    shared buffer then all I need is to read string from file and move
    pointer to next free byte in buffer.  With that I can have whole logic
    in main() function.
    
    Also I found better path to BAT0 device and with that I no longer feel
    like I need macros for paths.

--- dwmsss.c
+++ dwmsss.c
@@ -1,50 +1,30 @@
-#include <stdio.h>      /* DWM System Status String v1.4   sss  / / */
+#include <stdio.h>      /* DWM System Status String v2.0   sss  / / */
 #include <stdlib.h>     /* From: github.com/ir33k/dwmsss     __/ /  */
 #include <time.h>       /* Public domain (unlicense.org)  >~('__/   */
 
-#define TMP	"/tmp/dwmsss"
-#define BAT	"/sys/bus/acpi/drivers/battery/PNP0C0A:00/power_supply/BAT0"
-#define WLP     "/sys/class/net/wlp3s0"
-
 /* Read first word from file under PATH into BUF or SIZ as string.
- * ASCII characters smaller than 48 ('0') are consider delimiters. */
-void word(const char *path, char *buf, int siz) {
+ * ASCII characters smaller than 48 ('0') are consider delimiters.
+ * Return pointer after null terminator. */
+char *word(char *buf, int siz, const char *path) {
 	FILE *fp = fopen(path, "r");
-	if (!fp) return;
+	if (!fp) return buf;
 	while (--siz && (*buf = getc(fp)) > 47) buf += 1;
 	*buf = 0;
 	fclose(fp);
-}
-
-static char *wifi(void) {
-	static char buf[8];
-	word(WLP"/operstate", buf, 8);
-	return buf;
-}
-
-static char *volume(void) {
-	static char buf[8];
-	system("amixer get Master | grep -om1 '[0-9]\\+%' >"TMP);
-	word(TMP, buf, 8);
-	return buf;
-}
-
-static char *battery(void) {
-	static char buf[8];
-	word(BAT"/status", buf, 2);
-	word(BAT"/capacity", buf+1, 6);
-	buf[0] = buf[0] == 'C' ? '+' : '-';
-	return buf;
-}
-
-static char *date(void) {
-	static char buf[32];
-	time_t now = time(0);
-	strftime(buf, 32, "%m-%d %H:%M", localtime(&now));
-	return buf;
+	return buf+1;
 }
 
 int main(void) {
-	return printf(" %s@ %sv %sb | %s",
-		wifi(), volume(), battery(), date()) < 0;
+	char buf[64], *bp = buf, *wifi, *vol, *bat_s, *bat_c, *date;
+	time_t now = time(0);
+
+	system("amixer get Master | grep -om1 '[0-9]\\+%' > /tmp/dwmsss");
+	bp = word(vol   = bp, 8, "/tmp/dwmsss");
+	bp = word(wifi  = bp, 8, "/sys/class/net/wlp3s0/operstate");
+	bp = word(bat_s = bp, 8, "/sys/class/power_supply/BAT0/status");
+	bp = word(bat_c = bp, 8, "/sys/class/power_supply/BAT0/capacity");
+	strftime(date = bp, 16, "%m-%d %H:%M", localtime(&now));
+
+	return printf(" %s@ %sv %c%sb | %s",
+		wifi, vol, *bat_s == 'C' ? '+' : '-', bat_c, date) < 0;
 }
